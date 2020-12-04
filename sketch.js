4405c48546b726f066fd6b9e6c4ba42ed800c2cb
MihirP30 <mihir.pullakhandam@gmail.com>
2020-12-03 19:54:23 -0500

    created constraints

--- sketch.js
+++ sketch.js
@@ -1,10 +1,13 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
 
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
+
+var constrainedLog
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -14,7 +17,6 @@ function setup(){
     var canvas = createCanvas(1200,400);
     engine = Engine.create();
     world = engine.world;
-
 
     ground = new Ground(600,height,1200,20);
     platform = new Ground(150, 305, 300, 170);
@@ -36,6 +38,9 @@ function setup(){
 
     bird = new Bird(100,100);
 
+    constrainedLog = new Log(230,180,80,PI/2);
+
+    chain = new Chain(bird.body, constrainedLog.body);
 }
 
 function draw(){
@@ -60,5 +65,9 @@ function draw(){
     log5.display();
 
     bird.display();
+    
     platform.display();
+
+    constrainedLog.display();
+    chain.show();
 }
